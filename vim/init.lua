1a15d99152f9076da404ab01a65cfa65dcd32eb5
Aidan Lowson <dev@aidanlowson.com>
2024-10-30 21:34:04 +0000

    Formatting on save and leader f

--- vim/init.lua
+++ vim/init.lua
@@ -176,3 +176,7 @@ vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
 
+-- Keymap to format code using the lsp
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
+vim.cmd [[autocmd BufWritePre *.ts,*.tsx,*.js,*.jsx lua vim.lsp.buf.format()]]
+
