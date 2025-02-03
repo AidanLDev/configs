15ad6287591e2d38ae56e0f9373543249bcd5f5b
Aidan Lowson <dev@aidanlowson.com>
2025-02-03 21:42:21 +0000

    Use leader enter to check for code actions

--- vim/init.lua
+++ vim/init.lua
@@ -182,7 +182,7 @@ lspconfig.rust_analyzer.setup({
 
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set({ "n", "v" }, "<leader><CR>", vim.lsp.buf.code_action, {})
 
 -- Keymap to format code using the lsp
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
